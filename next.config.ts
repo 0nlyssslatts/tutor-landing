f361b27c0662c46cb84c23fad4ef7e36f14a884f
0nlyssslatts <sid36205@mail.ru>
2025-08-14 08:20:41 +0300

    Обновил конфиг для свг

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: {
+                loader: "@svgr/webpack",
+                options: {
+                    svgo: true,
+                },
+            },
+        });
+
+        return config;
+    },
     turbopack: {
         rules: {
             "*.svg": {
